7d5361c55c3d2e5ec98935d4714ebae1b08e2fc4
ncb6206 <ncb6206@gmail.com>
2023-11-06 19:33:08 +0900

    Feat : button component 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import NaverLoginButton from '@/app/components/NaverLoginButton';
+
 export default function Home() {
   return (
     <main className="flex h-full flex-col text-white">
       <div className="mx-auto mt-4">밥정너</div>
       <div className="flex-1"></div>
-      <div className="p-2 "></div>
+      <div className="p-2 ">
+        <NaverLoginButton />
+      </div>
     </main>
   );
 }
