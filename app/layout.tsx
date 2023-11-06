5d11d9a7a47a07a419d5341c51453880a6a5b81b
ncb6206 <ncb6206@gmail.com>
2023-11-06 19:18:01 +0900

    Init : 프로젝트 초기설정

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import type { Metadata } from 'next';
+import { Noto_Sans_KR } from 'next/font/google';
+import './globals.css';
+
+const font = Noto_Sans_KR({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: '밥정너',
+  description: '밥은 정해져있고 너만 답하면 돼',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <div
+          id="container"
+          className="relative mx-auto h-[100dvh] max-h-[926px] w-full max-w-[428px] overflow-y-hidden bg-[#282828]"
+        >
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex h-full flex-col text-white">
+      <div className="mx-auto mt-4">밥정너</div>
+      <div className="flex-1"></div>
+      <div className="p-2 "></div>
+    </main>
+  );
+}
