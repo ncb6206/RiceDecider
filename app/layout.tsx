fdc1f5883b7c429632e6b540e4605934a6949a68
ncb6206 <ncb6206@gmail.com>
2023-11-06 19:32:43 +0900

    Feat : Toast 기능 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import type { Metadata } from 'next';
 import { Noto_Sans_KR } from 'next/font/google';
+
+import Toast from '@/app/components/Toast';
+
 import './globals.css';
 
 const font = Noto_Sans_KR({ subsets: ['latin'] });
@@ -23,6 +26,7 @@ export default function RootLayout({
           className="relative mx-auto h-[100dvh] max-h-[926px] w-full max-w-[428px] overflow-y-hidden bg-[#282828]"
         >
           {children}
+          <Toast />
         </div>
       </body>
     </html>
