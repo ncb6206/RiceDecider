b03bf093ea4ca4d49180fd08b921390d554c9b43
ncb6206 <ncb6206@gmail.com>
2023-11-08 04:10:02 +0900

    Refactor : 커스텀 폰트, 색깔 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,9 +15,40 @@ const config: Config = {
       },
       colors: {
         rose: {
+          700: '#D53330',
+          600: '#E43D31F7',
           500: '#E14E4B',
+          400: '#D96E6E',
+          300: '#E69696',
+          200: '#FACACF',
+          100: '#FDEAED',
+        },
+        lime: {
+          500: '#D6EE23',
+          400: '#E4F66E',
+          300: '#ECF999',
+          200: '#F4FAC2',
+          100: '#FBFEE7',
+        },
+        pink: {
+          300: '#FFC3D8',
+          200: '#FCE9E9',
+        },
+        sky: {
+          300: '#A0DCFF',
+          200: '#DDF5FF',
+        },
+        green: {
+          200: '#D5F5D4',
+        },
+        Amber: {
+          100: '#F7FCD3',
         },
       },
+    },
+    fontFamily: {
+      Pretendard: ['Pretendard'],
+      SBAggro: ['SBAggro'],
     },
   },
   plugins: [],
