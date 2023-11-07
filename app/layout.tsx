b084ae0eecfc7741ca6bc91a7eaf1668bb6cbcc1
ncb6206 <ncb6206@gmail.com>
2023-11-07 23:59:41 +0900

    Feat : next-auth 기능 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,13 @@ import { Noto_Sans_KR } from 'next/font/google';
 import Toast from '@/app/components/Toast';
 
 import './globals.css';
+import NextAuthProvider from '@/Providers/NextAuthProvider';
 
 const font = Noto_Sans_KR({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: '밥정너',
-  description: '밥은 정해져있고 너만 답하면 돼',
+  description: '밥은 정해져있어, 너는 먹기만 해!',
 };
 
 export default function RootLayout({
@@ -19,15 +20,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body className={font.className}>
-        <div
-          id="container"
-          className="relative mx-auto h-[100dvh] max-h-[926px] w-full max-w-[428px] overflow-y-hidden bg-[#282828]"
-        >
-          {children}
-          <Toast />
-        </div>
+        <NextAuthProvider>
+          <div
+            id="container"
+            className="relative mx-auto h-[100dvh] max-h-[926px] w-full max-w-[428px] overflow-y-hidden border-[1px] border-black bg-white text-gray-900"
+          >
+            {children}
+            <Toast />
+          </div>
+        </NextAuthProvider>
       </body>
     </html>
   );
