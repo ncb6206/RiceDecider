78856a64c6a12621ade01ed1bcc71c67087b239d
ncb6206 <ncb6206@gmail.com>
2023-11-07 23:11:37 +0900

    Chore : 스크립트 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        rose: {
+          500: '#E14E4B',
+        },
+      },
     },
   },
   plugins: [],
