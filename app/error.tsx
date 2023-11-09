31a2ed980c22740a127dca4d85c8746ea3e2f61b
ncb6206 <ncb6206@gmail.com>
2023-11-10 02:20:40 +0900

    Feat : 에러페이지 추가

--- app/error.tsx
+++ app/error.tsx
@@ -1,7 +1,14 @@
 'use client';
 
+import Logo from '@/public/logo.svg';
+
 const ErrorPage = () => {
-  return <div>에러가 발생하였습니다!</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <Logo fill="#E14E4B" width="100" height="100" />
+      에러가 발생하였습니다!
+    </div>
+  );
 };
 
 export default ErrorPage;
---
+++ app/not-found.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import Logo from '@/public/logo.svg';
+
+const NotFound = () => {
+  const router = useRouter();
+
+  const goHome = () => {
+    router.push('/');
+  };
+
+  return (
+    <div className="flex h-full flex-col items-center justify-center gap-2">
+      <Logo
+        fill="#E14E4B"
+        width="100"
+        height="100"
+        onClick={goHome}
+        className="hover:cursor-pointer"
+      />
+      <p className="text-xl font-bold">에러가 발생하였습니다!</p>
+    </div>
+  );
+};
+
+export default NotFound;
