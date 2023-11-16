077a5364872dd04e45190c5a7ea793d9c1cd0390
ncb6206 <ncb6206@gmail.com>
2023-11-17 03:44:20 +0900

    Refactor : 에러페이지 수정

--- app/error.tsx
+++ app/error.tsx
@@ -1,12 +1,26 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 import Logo from '@/public/logo.svg';
 
 const ErrorPage = () => {
+  const router = useRouter();
+
+  const goHome = () => {
+    router.push('/');
+  };
+
   return (
-    <div className="flex flex-col gap-2">
-      <Logo fill="#E14E4B" width="100" height="100" />
-      에러가 발생하였습니다!
+    <div className="flex h-full flex-col items-center justify-center gap-2">
+      <Logo
+        fill="#E14E4B"
+        width="100"
+        height="100"
+        onClick={goHome}
+        className="hover:cursor-pointer"
+      />
+      <p className="text-xl font-bold">에러가 발생하였습니다!</p>
     </div>
   );
 };
