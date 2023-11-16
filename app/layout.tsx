fb67922ea0fabbea1980a21763be083f9bb54e43
ncb6206 <ncb6206@gmail.com>
2023-11-17 03:41:24 +0900

    Refactor : tanstack query 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,10 @@ import type { Metadata } from 'next';
 import { Noto_Sans_KR } from 'next/font/google';
 
 import Toast from '@/app/components/Toast';
+import ModalsProvider from '@/app/providers/ModalsProvider';
+import QueryProvider from '@/app/providers/QueryProvider';
 
 import './globals.css';
-import ModalsProvider from '@/app/providers/ModalsProvider';
 
 const font = Noto_Sans_KR({ subsets: ['latin'] });
 
@@ -22,14 +23,16 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={font.className}>
-        <div
-          id="container"
-          className="relative mx-auto h-[100dvh] max-h-[926px] w-full max-w-[428px] select-none overflow-x-hidden overflow-y-hidden bg-white font-Pretendard text-gray-900"
-        >
-          {children}
-          <Toast />
-          <ModalsProvider />
-        </div>
+        <QueryProvider>
+          <div
+            id="container"
+            className="relative mx-auto h-[100dvh] max-h-[926px] w-full max-w-[428px] select-none overflow-x-hidden overflow-y-hidden bg-white font-Pretendard text-gray-900"
+          >
+            {children}
+            <Toast />
+            <ModalsProvider />
+          </div>
+        </QueryProvider>
       </body>
     </html>
   );
---
+++ app/providers/QueryProvider.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { ReactNode, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
+
+interface QueryProviderProps {
+  children: ReactNode;
+}
+
+const QueryProvider = ({ children }: QueryProviderProps) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            retry: false,
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
