b2cd2d56e44139de98339c8e71b38fa3251ed5b7
ncb6206 <ncb6206@gmail.com>
2023-12-06 02:34:46 +0900

    Refactor : 토큰 테스트

--- app/services/token.ts
+++ app/services/token.ts
@@ -1,27 +1,20 @@
+import instance from './config/config';
+
 interface getTokenProps {
   code: string;
   state: string;
 }
 
 const getToken = async ({ code, state }: getTokenProps) => {
-  const NAVER_CALLBACK_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/login/oauth2/code/naver`;
-
   try {
-    const res = await fetch(
-      `${NAVER_CALLBACK_URL}?code=${code}&state=${state}`,
-      {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-        },
-        cache: 'no-store',
-      },
+    const res = await instance.get(
+      `/login/oauth2/code/naver?code=${code}&state=${state}`,
     );
 
-    if (!res.ok) throw new Error('Failed to fetch data');
+    if (res.status !== 200) throw new Error('Failed to fetch data');
 
-    const access_token = res.headers.get('Authorization');
-    const refresh_token = res.headers.get('Authorization-refresh');
+    const access_token = res.headers.Authorization;
+    const refresh_token = res.headers['Authorization-refresh'];
 
     console.log(access_token, refresh_token);
 
