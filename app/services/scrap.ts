3217e06327b6b06c6eb38159a7eac8b8ede13511
ncb6206 <ncb6206@gmail.com>
2023-12-08 03:01:28 +0900

    Refactor : 스크랩 로직 설정

---
+++ app/services/scrap.ts
@@ -0,0 +1,65 @@
+interface postScrapProps {
+  scrap: {
+    category: string;
+    realCategory: string;
+    title: string;
+    ttwwfew: string;
+    detailURL: string;
+    address: string;
+    radAddress: string;
+  };
+  access_token: string;
+}
+
+export const getScrapList = async (access_token: string) => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/scrapList`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      },
+    );
+
+    if (!res.ok) throw new Error('Failed to fetch data');
+
+    const data = await res.json();
+    console.log(data);
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const postScrap = async ({ scrap, access_token }: postScrapProps) => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/scrap`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${access_token}`,
+      },
+      body: JSON.stringify({
+        category: scrap.category,
+        realCategory: scrap.realCategory,
+        title: scrap.title,
+        ttwwfew: scrap.ttwwfew,
+        detailURL: scrap.detailURL,
+        address: scrap.address,
+        radAddress: scrap.radAddress,
+      }),
+    });
+
+    if (!res.ok) throw new Error('Failed to fetch data');
+
+    const data = await res.json();
+    // console.log(data);
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
