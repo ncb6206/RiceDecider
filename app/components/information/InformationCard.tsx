4bfaea2d321edf9088f949cf5dbb1ceae686090b
ncb6206 <ncb6206@gmail.com>
2023-12-10 04:14:42 +0900

    Refactor : react slick 설정

--- app/components/information/InformationCard.tsx
+++ app/components/information/InformationCard.tsx
@@ -1,42 +1,51 @@
 'use client';
 
 import Image from 'next/image';
+import Slider from 'react-slick';
 
+import '@/app/slick.css';
 import InformationKeyword from '@/app/components/information/informationKeyword';
 import useRecommendStore from '@/app/hooks/useRecommend';
+import SlickSetting from '@/app/components/slickSetting';
 
 const InformationCard = () => {
   const useRecommend = useRecommendStore(state => state);
+  const { title, category, address, roadAddress, imageZip } =
+    useRecommend.information;
 
   return (
     <div className="w-full">
-      {useRecommend.recommend.imageSrc && (
-        <Image
-          src={useRecommend.recommend.imageSrc}
-          alt="매장 사진"
-          width={400}
-          height={400}
-          className="h-[22rem] w-full"
-        />
-      )}
+      <Slider {...SlickSetting('information_dots')}>
+        {imageZip.length !== 0 &&
+          imageZip?.map((imageUrl, i) => (
+            <Image
+              key={i}
+              src={imageUrl.link}
+              alt="매장 사진"
+              width={400}
+              height={400}
+              className="h-[22rem] w-full"
+            />
+          ))}
+      </Slider>
       <div className="flex flex-col gap-3 p-6 font-medium">
         <p
           className="text-lg font-semibold text-gray-900 "
-          dangerouslySetInnerHTML={{ __html: useRecommend.recommend.title }}
+          dangerouslySetInnerHTML={{ __html: title }}
         />
         <div className="flex gap-2">
-          <InformationKeyword label={useRecommend.recommend.category || ''} />
+          <InformationKeyword label={category || ''} />
           {/* {keywords.map((keyword, i) => {
             return <InformationKeyword key={i} label={keyword} />;
           })} */}
         </div>
         <div className="flex flex-col gap-2">
           <p className="text-gray-600">주소</p>
-          <p className="underline">{useRecommend.recommend.address}</p>
+          <p className="underline">{address}</p>
         </div>
         <div className="flex flex-col gap-2">
           <p className="text-gray-600">도로명주소</p>
-          <p>{useRecommend.recommend.roadAddress}</p>
+          <p>{roadAddress}</p>
         </div>
       </div>
     </div>
