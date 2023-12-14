af09ba06f8b8429fc08f75e90fcd92d0142b80ae
ncb6206 <ncb6206@gmail.com>
2023-12-15 02:37:04 +0900

    Refactor : 로그인 되어있는 경우 로직 수정

--- app/components/category/CategoryHeader.tsx
+++ app/components/category/CategoryHeader.tsx
@@ -2,23 +2,39 @@
 
 import { useRouter } from 'next/navigation';
 import { AiOutlineHeart } from 'react-icons/ai';
+import { useEffect } from 'react';
+import { BiSolidHome } from 'react-icons/bi';
 
 import Header from '@/app/components/Header';
 import useLoginModal from '@/app/hooks/useLoginModal';
-import { isToken } from '@/app/utils/isToken';
+import useTokenStore from '@/app/hooks/useToken';
 
 const CategoryHeader = () => {
   const useLogin = useLoginModal();
-  const hasToken = isToken();
+  const { hasToken, isLogin } = useTokenStore();
   const router = useRouter();
 
+  useEffect(() => {
+    hasToken();
+  }, [hasToken]);
+
+  const onClickHome = () => {
+    router.push('/');
+  };
+
   const useGoScrap = () => {
-    if (hasToken) return router.push('/scrap');
+    if (isLogin) {
+      router.push('/scrap');
+      router.refresh();
+      return;
+    }
     useLogin.onOpen();
   };
 
   return (
     <Header
+      leftIcon={BiSolidHome}
+      leftOnClick={onClickHome}
       rightIcon={AiOutlineHeart}
       rightOnClick={useGoScrap}
       className="py-5"
