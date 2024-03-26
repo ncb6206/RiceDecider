fddf918e4adb0f8e8f6414117436587e90c3dba1
ncb6206 <ncb6206@gmail.com>
2024-03-26 14:02:23 +0900

    Refactor : information 상태 관리 zustand -> tanstack query로 변경

--- app/(routes)/information/[informationId]/_component/InformationCard.tsx
+++ app/(routes)/information/[informationId]/_component/InformationCard.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import Image from 'next/image';
 import Slider from 'react-slick';
 import { useQuery } from '@tanstack/react-query';
@@ -7,54 +5,65 @@ import { useParams } from 'next/navigation';
 
 import '@/app/slick.css';
 import InformationKeyword from '@/app/(routes)/information/[informationId]/_component/informationKeyword';
-import { imageZipProps, recommendProps } from '@/app/store/recommend';
 import SlickSetting from '@/app/components/slickSetting';
 import { getInformation } from '@/app/services/information';
 import { getImages } from '@/app/services/image';
 import clearWord from '@/app/utils/clearWord';
+import { Skeleton } from '@/app/components/ui/skeleton';
+import { RestaurantProps } from '@/app/models/Restaurant';
+import parseInformationId from '@/app/utils/parseInformationId';
+import { imageZipProps } from '@/app/models/Image';
 
 const InformationCard = () => {
   const params = useParams();
-  const [locationParam, titleParam] = decodeURI(
+  const { locationParam, titleParam } = parseInformationId(
     params.informationId as string,
-  ).split('%26');
-
-  const { data: informationList } = useQuery({
-    queryKey: ['information'],
-    queryFn: () => getInformation(`${locationParam} ${titleParam}`, 'client'),
-  });
-
-  const { data: imageZipData } = useQuery({
-    queryKey: ['information', 'image'],
-    queryFn: () => getImages(`${titleParam}`),
-  });
-
-  const informationData: recommendProps = informationList?.items?.find(
-    (item: recommendProps) => clearWord(item.title) === clearWord(titleParam),
   );
 
-  const imageZip: imageZipProps[] = imageZipData?.documents;
+  const { data: informationList } = useQuery({
+    queryKey: ['information', `${locationParam} ${titleParam}`],
+    queryFn: getInformation,
+    staleTime: 120 * 1000,
+    gcTime: 300 * 1000,
+  });
+
+  const { data: imageZipData, isLoading } = useQuery({
+    queryKey: ['information', 'image', `${titleParam}`],
+    queryFn: getImages,
+    staleTime: 120 * 1000,
+    gcTime: 300 * 1000,
+  });
+
+  const informationData = informationList?.items?.find(
+    (item: RestaurantProps) => clearWord(item.title) === clearWord(titleParam),
+  );
+
+  const imageZip = imageZipData?.documents;
 
   return (
     <div className="w-full">
-      <Slider {...SlickSetting('information_dots')}>
-        {imageZip?.length !== 0 &&
-          imageZip?.map((imageUrl: imageZipProps, i: number) => (
-            <Image
-              key={i}
-              src={imageUrl.image_url}
-              alt="매장 사진"
-              loading="lazy"
-              width={400}
-              height={400}
-              className="h-[22rem] w-full"
-            />
-          ))}
-      </Slider>
+      <div className="w-full">
+        {isLoading && !imageZip && <Skeleton className="h-[22rem] w-full" />}
+        <Slider {...SlickSetting('information_dots')}>
+          {imageZip &&
+            imageZip?.map((imageUrl: imageZipProps, i: number) => (
+              <div key={i} className="relative h-[22rem] w-full">
+                <Image
+                  key={i}
+                  src={imageUrl.image_url}
+                  alt="매장 사진"
+                  fill
+                  sizes="300px"
+                  priority
+                />
+              </div>
+            ))}
+        </Slider>
+      </div>
       <div className="flex flex-col gap-3 p-6 font-medium">
         <p
           className="text-lg font-semibold text-gray-900 "
-          dangerouslySetInnerHTML={{ __html: informationData?.title }}
+          dangerouslySetInnerHTML={{ __html: informationData?.title as string }}
         />
         <div className="flex gap-2">
           <InformationKeyword label={informationData?.category || ''} />
--- app/(routes)/information/[informationId]/page.tsx
+++ app/(routes)/information/[informationId]/page.tsx
@@ -2,6 +2,7 @@ import { QueryClient } from '@tanstack/react-query';
 
 import InformationClient from './InformationClient';
 import { getInformation } from '@/app/services/information';
+import parseInformationId from '@/app/utils/parseInformationId';
 // import ErrorPage from '@/app/error';
 
 const InformationPage = async ({
@@ -10,11 +11,13 @@ const InformationPage = async ({
   params: { informationId: string };
 }) => {
   const queryClient = new QueryClient();
-  const [location, title] = decodeURI(params.informationId).split('%26');
+  const { locationParam: location, titleParam: title } = parseInformationId(
+    params.informationId,
+  );
 
   await queryClient.prefetchQuery({
-    queryKey: ['information'],
-    queryFn: () => getInformation(`${location} ${title}`, 'server'),
+    queryKey: ['information', `${location} ${title}`],
+    queryFn: getInformation,
   });
 
   // if (infomation.items.length === 0) {
